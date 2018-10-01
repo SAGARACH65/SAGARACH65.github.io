1307e757ca91a89f9a41746b898475b01e96f016
Sagar Acharya <sagarach65@gmail.com>
2018-10-01 06:42:08 +0545

    refactored as6

---
+++ js-experiments/ast6/js/Background.js
@@ -0,0 +1,60 @@
+function Background() {
+    //checks the y position of the lane 
+    this.laneTracker = -canvas.height
+
+
+    var drawSideLanes = function () {
+
+        //this version is for moving side lanes
+        var currentY = this.laneTracker;
+        for (var i = 0; i < 8; i++) {
+            //drawing side-left lane
+            drawRect(0, currentY, LANE_X_POS[0] - 2, 20, ' #388E3C');
+            drawRect(0, currentY + 20, LANE_X_POS[0] - 2, 20, ' #4CAF50');
+            //drawing right side lane
+            drawRect(LANE_X_POS[3] + 4, currentY, LANE_X_POS[0] + 2, 20, ' #388E3C');
+            drawRect(LANE_X_POS[3] + 4, currentY + 20, LANE_X_POS[0] + 2, 20, ' #4CAF50');
+
+            currentY += 40;
+        }
+
+        //    // for static side lanes
+        //     drawRect(0, 0, LANE_X_POS[0] - 2, canvas.height, ' #C6C7CB');
+        //     drawRect(LANE_X_POS[3] + 4, 0, LANE_X_POS[0], canvas.height, ' #C6C7CB');
+    }
+
+    /**
+     * individual lane refers to the single strip of lines running from the start to end
+     * @param {number} x :the x coordinate of the individual lane 
+     * @param {string} color: the color of the box to make(either yellow or white) 
+     */
+    var = drawIndividualLane = function (x, color) {
+
+        var currentY = this.laneTracker;
+        for (var i = 0; i < NO_OF_LANE_DIVIDERS * 2; i++) {
+            drawRect(x, currentY + x / 10, LANE_DIVIDER_WIDTH, LANE_DIVIDER_HEIGHT, color);
+            currentY += LANE_DIVIDER_HEIGHT + LANE_DIVIDER_SPACING;
+        }
+        //for resetting the position of the lanes at certain intervals
+        if (this.laneTracker > -20) this.laneTracker = -canvas.height;
+    }
+
+    var drawLanes = function () {
+        //yellow separator between the left sideLane and the road
+        drawIndividualLane(LANE_X_POS[0], '#B89E4A');
+
+        //the white separators between lanes making the 3 lanes
+        drawIndividualLane(LANE_X_POS[1], 'white');
+        drawIndividualLane(LANE_X_POS[2], 'white');
+
+        //yellow separator between the right sideLane and the road
+        drawIndividualLane(LANE_X_POS[3], '#B89E4A');
+    }
+
+    this.draw = function () {
+        drawLanes();
+        //things that are present at the side of the road
+        drawSideLanes();
+        this.laneTracker += 1.2;
+    }
+};
--- js-experiments/ast6/js/Game.js
+++ js-experiments/ast6/js/Game.js
@@ -6,6 +6,7 @@ var isRightPressed = false,
     isLeftPressed = false;
 
 var mainCar = new Car(82, 123, 4, 'images/car4.png');
+var road=new Background();
 
 var OPPONENT_CAR_IMAGES = ['images/car2.png', 'images/car3.png', 'images/car1.png', 'images/car5.png'];
 var opponentCarsArr = [];
@@ -14,8 +15,7 @@ var ROAD_WIDTH = 600;
 //width at each side of the road.
 var SIDE_LANE_WIDTH = 100;
 var NO_OF_LANE_DIVIDERS = 4;
-//checks the y position of the lane  
-var laneTracker = -canvas.height;
+
 
 //between each other
 var LANE_DIVIDER_SPACING = 15;
@@ -121,61 +121,6 @@ var drawCars = function () {
     drawEnemyCars();
 }
 
-var drawSideLanes = function () {
-
-    //this version is for moving side lanes
-    var currentY = laneTracker;
-    for (var i = 0; i < 8; i++) {
-        //drawing side-left lane
-        drawRect(0, currentY, LANE_X_POS[0] - 2, 20, ' #388E3C');
-        drawRect(0, currentY + 20, LANE_X_POS[0] - 2, 20, ' #4CAF50');
-        //drawing right side lane
-        drawRect(LANE_X_POS[3] + 4, currentY, LANE_X_POS[0] + 2, 20, ' #388E3C');
-        drawRect(LANE_X_POS[3] + 4, currentY + 20, LANE_X_POS[0] + 2, 20, ' #4CAF50');
-
-        currentY += 40;
-    }
-
-//    // for static side lanes
-//     drawRect(0, 0, LANE_X_POS[0] - 2, canvas.height, ' #C6C7CB');
-//     drawRect(LANE_X_POS[3] + 4, 0, LANE_X_POS[0], canvas.height, ' #C6C7CB');
-}
-
-/**
- * individual lane refers to the single strip of lines running from the start to end
- * @param {number} x :the x coordinate of the individual lane 
- * @param {string} color: the color of the box to make(either yellow or white) 
- */
-var drawIndividualLane = function (x, color) {
-
-    var currentY = laneTracker;
-    for (var i = 0; i < NO_OF_LANE_DIVIDERS * 2; i++) {
-        drawRect(x, currentY + x / 10, LANE_DIVIDER_WIDTH, LANE_DIVIDER_HEIGHT, color);
-        currentY += LANE_DIVIDER_HEIGHT + LANE_DIVIDER_SPACING;
-    }
-    //for resetting the position of the lanes at certain intervals
-    if (laneTracker > -20) laneTracker = -canvas.height;
-}
-
-
-var drawLanes = function () {
-    //yellow separator between the left sideLane and the road
-    drawIndividualLane(LANE_X_POS[0], '#B89E4A');
-
-    //the white separators between lanes making the 3 lanes
-    drawIndividualLane(LANE_X_POS[1], 'white');
-    drawIndividualLane(LANE_X_POS[2], 'white');
-
-    //yellow separator between the right sideLane and the road
-    drawIndividualLane(LANE_X_POS[3], '#B89E4A');
-}
-
-var drawRoad = function () {
-    drawLanes();
-    //things that are present at the side of the road
-    drawSideLanes();
-    laneTracker += 1.2;
-}
 
 var showScore = function () {
     var scoreDiv = document.getElementsByClassName('score')[0];
@@ -197,7 +142,7 @@ var gameLoop = function () {
     ctx.fillStyle = '#333335';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    drawRoad();
+    road.draw();
 
     drawCars();
     updateCarsLocation();
